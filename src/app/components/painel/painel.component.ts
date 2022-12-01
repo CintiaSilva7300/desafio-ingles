b38314b127298d950ae72cdadedcdb5bbeafaf47
cintiasilva7300 <cintiasilva7300@gmail.com>
2022-12-01 12:35:10 -0300

    funcao atualizaResposta criada com verificacoes de acertos e erros

--- src/app/components/painel/painel.component.ts
+++ src/app/components/painel/painel.component.ts
@@ -9,20 +9,43 @@ import { FRASES } from './frases-mock';
   styleUrls: ['./painel.component.scss']
 })
 export class PainelComponent implements OnInit {
-public frases:Frase[] = FRASES
-public instrucao: string = 'Traduza a frase:'
-public resposta!: string;
+  public frases: Frase[] = FRASES
+  public instrucao: string = 'Traduza a frase:'
+  public resposta!: string;
 
-  constructor(){
-    console.log(this.frases) //objeto frase
+  public rodada: number = 0;
+  public rodadaFrase!: Frase;
+
+  constructor() {
+    //console.log(this.frases) //objeto frase
+    this.rodadaFrase = this.frases[this.rodada]
+    // console.log(this.rodadaFrase)
   }
 
   ngOnInit(): void {
 
-}
+  }
 
-atualizaResposta(resposta: Event): void {
-  this.resposta = (<HTMLInputElement>resposta.target).value
-  console.log(this.resposta)
-}
+  atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    // console.log(this.resposta)
+  }
+
+  verificarResposta(): void { //trocar pergunta da rodada
+
+    if (this.rodadaFrase.frasePortugues === this.resposta) {
+      alert('Correta')
+
+      this.rodada++
+
+      //ATUALIZA O OBJETO RODADAFRASE
+      console.log(this.rodada)
+      this.rodadaFrase = this.frases[this.rodada]
+      // console.log(this.rodadaFrase)
+
+    } else (
+      alert('Errada')
+    )
+  }
+
 }
